b34437baa51eaf34590889cf39c19a8e7a459f1c
Artur <artur.talimonchyk@gmail.com>
2024-12-06 16:07:33 +0100

    intialized project

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh','react-hooks'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react-hooks/rules-of-hooks': 'warn', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn' ,
+    'react/prop-types': 'warn',
+    'react/jsx-no-target-blank': 'error',
+  },
+}
